d60d7f9c751b2450b8ad3317f88b57d3c7d31138
RahafDuhaimesh <rahafduhaimesh211@gmail.com>
2024-06-26 21:36:01 +0300

    26

---
+++ 26June2024/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26June2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------TASK1----------");
+            //Console.WriteLine("Enter the first number : ");
+            //int int1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter the second number : ");
+            //int int2 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine($"The smaller number is : {Math.Min(int1,int2)}");
+
+            Console.WriteLine("----------TASK2----------");
+            //Console.WriteLine("Enter a number : ");
+            //int int3 = Convert.ToInt32(Console.ReadLine());
+            // method 1 :
+            //if (int3 >= 0)
+            //{
+            //    Console.WriteLine($"The sign is of {int3} is positive");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"The sign is of {int3} is negative");
+            //}
+            // method 2 :
+            //string sign = (int3>0) ? "+" : "-";
+            //Console.WriteLine($"The sign of {int3} is {sign}");
+
+            Console.WriteLine("----------TASK3----------");
+            //Console.WriteLine("ENTER 3 NUMBERS : ");
+            //int int4 = Convert.ToInt32(Console.ReadLine());
+            //int int5 = Convert.ToInt32(Console.ReadLine());
+            //int int6 = Convert.ToInt32(Console.ReadLine());
+            // method 1 : 
+            //int[] numbers = { int4, int5, int6 };
+            //Array.Sort(numbers);
+            //Console.WriteLine($"The numbers from the smallest to the biggest : {numbers[0]} {numbers[1]} {numbers[2]} ");
+            //Array.Reverse(numbers);
+            //Console.WriteLine($"The numbers from the biggest to the smallest : {numbers[0]} {numbers[1]} {numbers[2]} ");
+
+            // method 2 : 
+            //if (int4 < int5)
+            //{
+            //    int temp = int4;
+            //    int4 = int5;
+            //    int5 = temp;
+            //}
+            //if (int4 < int6)
+            //{
+            //    int temp = int4;
+            //    int4 = int6;
+            //    int6 = temp;
+            //}
+            //if (int5 < int6)
+            //{
+            //    int temp = int5;
+            //    int5 = int6;
+            //    int6 = temp;
+            //}
+
+            //Console.WriteLine($"The numbers sorted from largest to smallest are: {int4}, {int5}, {int6}");
+
+            Console.WriteLine("----------TASK4----------");
+            //int[] numbers = { 4, 9, -7, 15, 25 };
+            //int maxNum = numbers.Max();
+            //Console.WriteLine($"The max number is : {maxNum}");
+
+            Console.WriteLine("----------TASK5----------");
+            //Console.WriteLine("Please enter the kilometers that you want to convert : ");
+            //int int7 = Convert.ToInt32(Console.ReadLine());
+            //double milesPerHour = int7 * 0.6214;
+            //Console.WriteLine($"{int7} kilometers per hours are equal to {milesPerHour:F6} . ");
+
+            Console.WriteLine("----------TASK6----------");
+            //Console.WriteLine("Please enter the hours : ");
+            //int int8 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Please enter the minutes : ");
+            //int int9 = Convert.ToInt32(Console.ReadLine());
+            //int8 = int8 * 60;
+            //int sum = int8 + int9;
+            //Console.WriteLine($"The total minutes equal : {sum} ");
+
+            Console.WriteLine("----------TASK7----------");
+            //Console.WriteLine("Please enter the minutes : ");
+            //int int10= Convert.ToInt32(Console.ReadLine());
+            //int total = int10 / 60;
+            //int minutes = int10 % 60;
+            //Console.WriteLine($" {total} hours , {minutes} minutes ");
+
+            Console.WriteLine("----------TASK8----------");
+            //string[] strings = { "Hello world!", "The life is nice", "How are you?", "Im here waiting for the lunch", "is the lunch ready? " };
+            //Console.WriteLine(strings[0] + "\n" +"The length is : " +  strings[0].Length);
+            //Console.WriteLine(strings[1] + "\n" + "The length is : " + strings[1].Length);
+            //Console.WriteLine(strings[2] + "\n" + "The length is : " + strings[2].Length);
+            //Console.WriteLine(strings[3] + "\n" + "The length is : " + strings[3].Length);
+            //Console.WriteLine(strings[4] + "\n" + "The length is : " + strings[4].Length);
+
+        }
+    }
+}
