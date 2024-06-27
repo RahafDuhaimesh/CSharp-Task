477043868b59e467fb40be2459505ed57964187e
RahafDuhaimesh <rahafduhaimesh211@gmail.com>
2024-06-27 22:18:03 +0300

    27-6

---
+++ 27June2024/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27June2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" --------------- TASK 1 -----------");
+            //int[] ARR = { 1, 7, 9, 45 };
+            //string[] arr2 = {"Str", "alex", "moh"};
+            //string[] arr3 = { "the", "fox" ,"over", "lazy", "dog" };
+
+            Console.WriteLine(" --------------- TASK 2 -----------");
+            //String[] fruits = { "Tomato", "Banana", "Watermelon" };
+            //for (int i = 0; i < fruits.Length; i++)
+            //{
+            //    if (fruits[i] == "Tomato")
+            //    {
+            //        Console.WriteLine($"{fruits[i]} is found in index {i} .");
+            //    }
+            //    if (fruits[i] == "Banana")
+            //    {
+            //        Console.WriteLine($"{fruits[i]} is found in index {i} .");
+            //    }
+            //}
+
+            Console.WriteLine(" --------------- TASK 3 -----------");
+            //string[] foods = { "Tomato", "Banana", "Watermelon", "Mansaf", "Mandi" };
+            //string[] sports = { "running", "swimming", "walking" };
+            //string[] movies = { "black swan", "a man called otto", "the walking dead", "swans" };
+            //Console.WriteLine($"My favorite foods are :");
+            //foreach (string food in foods) {
+            //    Console.WriteLine (food);
+            //}
+            //Console.WriteLine("-------------------------");
+            //Console.WriteLine($"My favorite sports are :");
+            //foreach (string sport in sports)
+            //{
+            //    Console.WriteLine(sport);
+            //}
+            //Console.WriteLine("-------------------------");
+            //Console.WriteLine($"My favorite movies are :");
+            //foreach (string movie in movies)
+            //{
+            //    Console.WriteLine(movie);
+            //}
+
+            Console.WriteLine(" --------------- TASK 4 -----------");
+            //Console.WriteLine("Please enter three numbers : ");
+            //int firstNumber = Convert.ToInt32(Console.ReadLine());
+            //int secondNumber = Convert.ToInt32(Console.ReadLine());
+            //int thirdNumber = Convert.ToInt32(Console.ReadLine());
+            //int sum = 0;
+            //int[] numbers = { firstNumber, secondNumber, thirdNumber };
+            //foreach (int number in numbers)
+            //{
+            //    sum += number;
+            //}
+            //Console.WriteLine($"The numbers you enterd are : {firstNumber} , {secondNumber} , {thirdNumber}");
+            //Console.WriteLine($"The sum of the three numbers is {sum}");
+
+            Console.WriteLine(" --------------- TASK 5 -----------");
+            //int sumOfOdd = 0;
+            //Console.WriteLine("The odd numbers are : ");
+            //for (int i = 1; i <= 100; i+=2)
+            //{
+            //    sumOfOdd = sumOfOdd + i;
+            //    if (i == 99) { 
+            //    Console.WriteLine($"{i}.\n");
+            //    }
+            //    else
+            //    {
+            //        Console.Write($"{i},");
+            //    }
+            //}
+            //Console.WriteLine($"The Sum of odd Numbers is: {sumOfOdd}");
+
+            Console.WriteLine(" --------------- TASK 6 -----------");
+            //-------------Method 1------------- :
+            //string star = "*****";
+            //for (int i = 1; i <= 4; i++) {
+            //    Console.WriteLine(star.Substring(1, i));
+            //}
+            //------------Method 2-------------- :
+            //for (int i = 1; i <= 4; i++)
+            //{
+            //    for (int j = 0; j < i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+            //------------Method 3-------------- :
+            //int n = 4; 
+            //for (int i = 1; i <= n; i++) 
+            //{
+            //    for (int j = i; j < n; j++) 
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for (int k = 1; k <= i; k++) 
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            Console.WriteLine(" --------------- TASK 7 -----------");
+            //int rows = 4;
+            //int number = 1;
+            //for (int i = 1; i <= rows; i++)
+            //{
+            //    for (int j = 1; j <= rows - i; j++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for (int k = 1; k <= i; k++)
+            //    {
+            //        Console.Write("{0} ", number);
+            //        number++;
+            //    }
+
+            //    Console.WriteLine(); 
+            //}
+        }
+
+    }
+    
+}
