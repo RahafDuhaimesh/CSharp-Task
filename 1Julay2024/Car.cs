88868029c462b540b3bfa04a914343304d762ee1
RahafDuhaimesh <rahafduhaimesh211@gmail.com>
2024-07-01 22:32:43 +0300

    1.7

---
+++ 1Julay2024/Car.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1Julay2024
+{
+    public class Car
+    {
+        // fields
+        public string Make;
+        public int Year;
+        public string Type;
+        public double Price;
+        public string Model;
+        public int PalletNo;
+        public string Color;
+
+        // Constructor
+        public Car()
+        {
+            
+        }
+        public Car(string make, int year , string type, double price, string model, int palletNo, string color)
+        {
+            this.Make = make;
+            this.Year = year;
+            this.Type = type;
+            this.Price = price;
+            this.Model = model;
+            this.PalletNo = palletNo;
+            this.Color = color;
+
+        }
+
+        //Methods 
+        public void Start()
+        {
+            Console.WriteLine($"The engine of {Make} {Year} starts to running.");
+        }
+        public void Stop()
+        {
+            Console.WriteLine($"The engine of {Make} {Year} ends running.");
+        }
+        public string GetCarInfo()
+        {
+            return $"Make: {Make}, Year: {Year}, Type: {Type}, Price: {Price}, Model: {Model}, Pallet No: {PalletNo}, Color: {Color}";
+        }
+
+    }
+    public class BMW:Car
+    {
+        public BMW(string make, int year, string type, double price, string model, int palletNo, string color)
+        {
+            this.Make = make;
+            this.Year = year;
+            this.Type = type;
+            this.Price = price;
+            this.Model = model;
+            this.PalletNo = palletNo;
+            this.Color = color;
+        }
+
+
+    }
+
+}
---
+++ 1Julay2024/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1Julay2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car("Kia",2000,"Serato",4400,"vip",5599,"white");
+            car1.Start();
+            Console.WriteLine(car1.GetCarInfo()); 
+            car1.Stop();
+            Console.WriteLine("---------------------------------------");
+            BMW car2 = new BMW("BMW", 2022, "SUV", 55000, "X5", 123456, "Black");
+            car2.Start();
+            Console.WriteLine(car2.GetCarInfo());
+            car2.Stop();
+        }
+    }
+}
