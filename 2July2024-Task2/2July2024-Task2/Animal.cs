31434a6ae197e7746dc2a3546fe90d47da082101
RahafDuhaimesh <rahafduhaimesh211@gmail.com>
2024-07-02 20:34:48 +0300

    2.7.2024

---
+++ 2July2024-Task2/2July2024-Task2/Animal.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2July2024_Task2
+{
+    public interface ISound
+    {
+        void MakeSound();
+    }
+    public abstract class Animal
+    {   // Property
+        public string Name { get; set; }
+
+        // Constructor 
+        public Animal(string name)
+        {
+            this.Name = name;
+        }
+        public Animal()
+        {
+            
+        }
+
+        // Abstract Method
+        public abstract  void Eat();
+
+        // Concrete Method 
+        public void Sleep()
+        {
+            Console.WriteLine($"{Name} is sleeping.");
+
+        }
+
+    }
+    public class Dog:Animal ,ISound
+    {
+        public void MakeSound()
+        {
+            Console.WriteLine($"The sound of dog is : \"Woof!\" .");
+        }
+        public override void Eat()
+        {
+            Console.WriteLine("The dog eats meat");
+        }
+        public Dog(string name) : base(name)
+        {
+        }
+
+
+    }
+    public class Cat : Animal, ISound
+    {
+        public void MakeSound()
+        {
+            Console.WriteLine($"The sound of cat is : \"Meow!\" .");
+        }
+        public override void Eat()
+        {
+            Console.WriteLine("The cat eats meat");
+        }
+        public Cat(string name) : base(name)
+        {
+        }
+    }
+}
---
+++ 2July2024-Task2/2July2024-Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2July2024_Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Dog dog = new Dog("Hasky");
+            dog.MakeSound();
+            dog.Sleep();
+            dog.Eat();
+            Console.WriteLine("---------------------------------");
+            Cat cat = new Cat("Baldy");
+            cat.MakeSound();
+            cat.Sleep();
+            cat.Eat();
+            Console.WriteLine("---------------------------------");
+
+        }
+    }
+}
