2a3e3be71a7b0d75ecbc80ae85a515fcde5ef5d2
RahafDuhaimesh <rahafduhaimesh211@gmail.com>
2024-07-02 20:33:45 +0300

    2.7.2024

---
+++ 2July2024-Task1/2July2024-Task1/Car.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2July2024_Task1
+{
+    public  class Car
+    {
+        // Properties
+        public string Make { get; set; }
+        public int Year { get; set; }
+        public string Type { get; set; }
+        public double Price { get; set; }
+        public int Model { get; set; }
+        public int PalletNo { get; set; }
+        public string Color { get; set; }
+
+        // Methods 
+        public virtual void Display()
+        {
+            Console.WriteLine("Welcome Car");
+        }
+        public void DisplayInfo(string make, int year)
+        {
+            Console.WriteLine($"The car is {make} made in year {year} .");
+        }
+        public void DisplayInfo(string make)
+        {
+            Console.WriteLine($"The car is {make} .");
+        }
+
+    }
+
+    public  class BMW : Car
+    {
+        public override void Display()
+        {
+            Console.WriteLine("Welcome BMW");
+        }
+    }
+}
---
+++ 2July2024-Task1/2July2024-Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2July2024_Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          Car car1 = new Car();
+            car1.Display();
+            car1.DisplayInfo("Kia");
+            car1.DisplayInfo("Kia", 1980);
+
+          BMW car2 = new BMW();
+            car2.Display();
+
+        }
+    }
+}
