45e98dcb2b2a7a802afc4e09015766287df2bb4e
RahafDuhaimesh <rahafduhaimesh211@gmail.com>
2024-07-03 20:42:07 +0300

    task1-3-7

---
+++ 3-7-2024-Task1/DataLoginPage.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="DataLoginPage.aspx.cs" Inherits="_3_7_2024_Task1.DataLoginPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+        <link rel="stylesheet" href="LoginForm.css"/>
+
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+              <center>
+        <div id="form" style="background-color: #C0C0C0">
+            <h2>Your Data !</h2>
+            <asp:Label ID="LabelName" runat="server" Text=""></asp:Label><br />
+            <asp:Label ID="LabelEmail" runat="server" Text=""></asp:Label><br />
+            <asp:Label ID="LabelID" runat="server" Text=""></asp:Label><br />
+            <asp:Label ID="LabelGender" runat="server" Text=""></asp:Label><br />
+            <asp:Label ID="LabelCourses" runat="server" Text=""></asp:Label><br />
+            <asp:Label ID="LabelDescription" runat="server" Text=""></asp:Label><br />
+        </div>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        </center>
+        </div>
+    </form>
+</body>
+</html>
---
+++ 3-7-2024-Task1/LoginForm.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _3_7_2024_Task1
+{
+    public partial class LoginForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+       
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string name = Text1.Text;
+            string email = Text2.Text;
+            string id = Text3.Text;
+            string gender = RadioButton1.Checked ? "Female" : (RadioButton2.Checked ? "Male" : "");
+            string url = $"SecondPage.aspx?name={name}&email={email}&id={id}&gender={gender}";
+            string courses = string.Empty;
+            if (CheckBox1.Checked)
+            {
+                courses += "C ";
+            }
+            if (CheckBox3.Checked)
+            {
+                courses += "C++ ";
+            }
+            if (CheckBox2.Checked)
+            {
+                courses += "C# ";
+            }
+
+
+            string box = TextBox1.Text;
+            string x = $"DataLoginPage.aspx?name={name}&email={email}&id={id}&gender={gender}&courses={courses}&box={box}";
+            Response.Redirect(x);
+
+        }
+
+        protected void RadioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
